aea7873b2e88cbc6a261a3a00f9381ca0b487c50
yutingh <yutingh@cw.com.tw>
2023-01-04 13:39:16 +0800

    content-visibility

---
+++ content-visibility/render.js
@@ -0,0 +1,11 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
+const app = createApp({
+  data() {
+    return {
+      test: 'teststetsedfdsfsdf',
+    }
+  },
+  methods: {
+  }
+});
+app.mount('#app');
